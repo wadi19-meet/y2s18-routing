c8d5fc832aa6991db5082b6a93f90991c9acb598
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 14:10:19 +0300

    routing is good

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,21 @@
 from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
-
+# @app.route('/')
+# def home1 () :
+#     return render_template(
+#         'student.html')
+    
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template(
+        'home.html')
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    student= session.query(student_id
+    ).first()
+    return render_template(
+        'student.html', n = student_id)        
 
 app.run(debug=True)
